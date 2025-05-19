cbf629f9f80deabc3089906724a784efcff19bae
이민주 <minju0236@gmail.com>
2025-05-19 16:17:50 +0900

    🧭 chore: navigation 타입 정의 수정
    MealDetail, AnalysisScreen 타입 명세 추가 및 params 연결

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,23 +1,48 @@
+export interface NutrientItem {
+  key: number;
+  grams: number;
+  color: string;
+  label: string;
+}
+
 export type RootStackParamList = {
-    Welcome: undefined;
-    FindAccount: undefined;
-    SignUp: undefined;
-    ProfileSetting: undefined;
-    SignupComplete: undefined;
+  Welcome: undefined;
+  FindAccount: undefined;
+  SignUp: undefined;
+  ProfileSetting: undefined;
+  SignupComplete: undefined;
 
-    Home: undefined;
+  Home: undefined;
 
-    Analysis: undefined;
-    Report: undefined;
-    PhotoPreview: { imageUri: string };
-    MealRecord: { imageUri: string };
-
-    Community: undefined
-
-    MyPage: undefined; 
-    ProfileEdit: undefined;
-    EditGoal: undefined;
-    EditIdNick: undefined;
-    EditPass: undefined;
+  Analysis: {
+    imageSource: { uri: string } | any;
+    title: string;
+    mealTime: string;
+    sugar: string;
+    protein: string;
+    tag: '과다' | '적정' | '부족';
   };
-  
+  Report: undefined;
+  PhotoPreview: { imageUri: string };
+  MealRecord: {
+    imageUri: string;
+    rawNutrients: NutrientItem[];
+    selectedMenu?: string;
+    selectedKcal?: number;
+  };
+  FoodSearch: {
+    imageUri: string;
+    rawNutrients: NutrientItem[];
+  };
+  MealDetail: {
+    imageUri: string;
+  };
+
+  Community: undefined;
+
+  MyPage: undefined;
+  ProfileEdit: undefined;
+  EditGoal: undefined;
+  EditIdNick: undefined;
+  EditPass: undefined;
+};
